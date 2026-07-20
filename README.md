39077545c6c982ce86b9da58d202df64f4569119
Chaitrali Supali <supalichaitrali@gmail.com>
2026-07-20 08:40:50 +0000

    Complete Linux System Health Monitor v1.0

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # Linux System Health Monitor
-A professional Bash-based Linux system monitoring project that collects essential system health information including CPU, memory, disk usage, uptime, logged-in users, and running processes. The project follows industry-standard Git and GitHub workflows and is designed to strengthen Linux Administration, Cloud, and DevOps skills.
+
+A Bash scripting project that monitors the overall health of a Linux system by collecting important system information such as CPU usage, memory usage, disk usage, system uptime, running processes, load average, user information, and network details.
+
+## Features
+
+- System Information
+- Memory Monitoring
+- CPU Monitoring
+- Disk Usage Monitoring
+- User Information
+- Running Processes
+- Load Average Monitoring
+- Network Information
+- Health Status Checks
+- Configuration File Support
+- Logging Support
+- Command-Line Arguments
+
+## Project Structure
+
+```
+Linux_System_Health_Monitor/
+├── README.md
+├── config/
+│   └── settings.conf
+├── docs/
+├── logs/
+│   └── Linux_System_Health_Monitor.log
+├── reports/
+└── scripts/
+    └── system_health_monitor.sh
+```
+
+## Usage
+
+Run complete report:
+
+```bash
+./scripts/system_health_monitor.sh --all
+```
+
+Run individual modules:
+
+```bash
+./scripts/system_health_monitor.sh --system
+./scripts/system_health_monitor.sh --memory
+./scripts/system_health_monitor.sh --cpu
+./scripts/system_health_monitor.sh --disk
+./scripts/system_health_monitor.sh --user
+./scripts/system_health_monitor.sh --process
+./scripts/system_health_monitor.sh --load
+./scripts/system_health_monitor.sh --network
+./scripts/system_health_monitor.sh --help
+```
+
+## Technologies Used
+
+- Bash Shell Scripting
+- Linux Commands
+- AWK
+- GREP
+- CUT
+- TOP
+- FREE
+- DF
+- PS
+- BC
+- Git
+- GitHub
+
+## Skills Demonstrated
+
+- Bash Scripting
+- Linux Administration
+- Shell Functions
+- Configuration Management
+- Log Management
+- Process Monitoring
+- System Monitoring
+- Git Version Control
+
+## Author
+
+**Chaitrali Supali**
--- scripts/system_health_monitor.sh
+++ scripts/system_health_monitor.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 LOG_DIR="logs"
 LOG_FILE="$LOG_DIR/Linux_System_Health_Monitor.log"
+
+mkdir -p "$LOG_DIR"
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
+
+log() {
+     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
+}
 
 CONFIG_FILE="config/settings.conf"
 
@@ -17,9 +24,9 @@ else
     exit 1
 fi
 
-echo "========================================" >> "$LOG_FILE"
-echo "Script started at : $(date)" >> "$LOG_FILE"
 exec > >(tee -a "$LOG_FILE") 2>&1
+log "Script started"
+
 #==========================================================================
 # Project : Linux System Health Monitor
 # Author  : Chaitrali Supali
@@ -39,6 +46,43 @@ check_command() {
   }
 }
 
+help_menu() {
+    echo
+    echo "Linux System Health Monitor"
+    echo
+    echo "Usage:"
+    echo "./scripts/system_health_monitor.sh --all"
+    echo "     Display complete system report"
+    echo
+    echo "./scripts/system_health_monitor.sh --system"
+    echo "     Display system information"
+    echo
+    echo "./scripts/system_health_monitor.sh --memory"
+    echo "     Display memory information"
+    echo
+    echo "./scripts/system_health_monitor.sh --cpu"
+    echo "     Display CPU information"
+    echo
+    echo "./scripts/system_health_monitor.sh --disk"
+    echo "     Display disk information"
+    echo
+    echo "./scripts/system_health_monitor.sh --user"
+    echo "     Display current user information"
+    echo
+    echo "./scripts/system_health_monitor.sh --process"
+    echo "     Display running processes"
+    echo
+    echo "./scripts/system_health_monitor.sh --load"
+    echo "     Display system load average"
+    echo
+    echo "./scripts/system_health_monitor.sh --network"
+    echo "     Display network information"
+    echo
+    echo "./scripts/system_health_monitor.sh --help"
+    echo "     Show this help message"
+    echo
+}
+ 
 # Displays basic system information.
 system_information(){
     HOSTNAME=$(hostname)
@@ -58,6 +102,7 @@ memory_information(){
     USED_MEMORY=$(free -h | awk 'NR==2 {print $3}')
     FREE_MEMORY=$(free -h | awk 'NR==2 {print $4}')
     AVAILABLE_MEMORY=$(free -h |awk 'NR==2 {print $7}')
+    MEMORY_USAGE=$(free | awk 'NR==2 {printf "%.0f", $3/$2 * 100}')
     echo
     echo "=========MEMORY INFORMATION========="
     echo "TOTAL MEMORY     : $TOTAL_MEMORY"
@@ -65,17 +110,38 @@ memory_information(){
     echo "FREE MEMORY      : $FREE_MEMORY"
     echo "AVAILABLE MEMORY : $AVAILABLE_MEMORY"
     echo
+    echo "=========MEMORY HEALTH=============="
+    echo "Memory Threshold : ${MEMORY_THRESHOLD}%"
+    echo "Memory Usage : ${MEMORY_USAGE}%"
+    
+    if [ "$MEMORY_USAGE" -gt "$MEMORY_THRESHOLD" ]
+    then
+        echo -e "Memory Status : ${RED}WARNING${NC}"
+    else
+        echo -e "Memory Status : ${GREEN}NORMAL${NC}"
+    fi
+    echo
 }
 # Displays CPU information.
 cpu_information(){
     CPU_ARCHITECTURE=$(lscpu | grep "Architecture" | awk '{print $2}')
     CPU_COUNT=$(lscpu | grep "^CPU(s): " | awk '{print $2}')
     CPU_MODEL=$(lscpu | grep "Model name" | cut -d':' -f2 | xargs)
+
+   CPU_USAGE=$(top -bn1 | awk -F'id,' '/Cpu/ {split($1,a,","); split(a[4],b," "); printf "%.0f",100-b[length(b)]}')
     echo
     echo "=============CPU INFORMATION=========="
     echo "Architecture : $CPU_ARCHITECTURE"
     echo "CPU Count : $CPU_COUNT"
     echo "CPU Model : $CPU_MODEL"
+    echo "CPU Usage : ${CPU_USAGE}%"
+    
+    if [ "$CPU_USAGE" -gt "$CPU_THRESHOLD" ]
+    then
+        echo -e "CPU Status : ${RED}HIGH CPU USAGE${NC}"
+    else
+        echo -e "CPU Status : ${GREEN}NORMAL${NC}"
+    fi
     echo
 }
 # Displays disk information.
@@ -95,9 +161,9 @@ disk_information(){
     DISK_USAGE_NUM=${DISK_USAGE%\%}
     if [ "$DISK_USAGE_NUM" -gt "$DISK_THRESHOLD" ] 
     then
-        echo -e "Status : ${RED}WARNING${NC}"
+        echo -e "DISK Status : ${RED}WARNING${NC}"
     else
-        echo -e "Status : ${GREEN}HEALTHY${NC}"
+        echo -e "DISK Status : ${GREEN}HEALTHY${NC}"
     fi
     echo
 }
@@ -116,19 +182,41 @@ user_information(){
 # Displays process information.
 process_information(){
     TOTAL_PROCESSES=$(ps -e --no-headers |wc -l)
-    TOP_PROCESSES=$(ps -ef | head -5)
+    
     echo
-    echo "======================RUNNING  PROCESSES================="
+    echo "======================RUNNING PROCESSES================="
     echo "Total Running Processes : $TOTAL_PROCESSES"
-    echo "TOP PROCESSES : $TOP_PROCESSES"
+    echo 
+    echo "Top 5 Processes:"
+    ps -eo pid,user,%cpu,%mem,comm --sort=-%cpu | head -6
+
+    echo
+
+    if [ "$TOTAL_PROCESSES" -gt "$PROCESS_THRESHOLD" ]
+    then
+        echo -e "Process Status : ${RED}HIGH PROCESS COUNT${NC}"
+    else
+        echo -e "Process Status : ${GREEN}NORMAL${NC}"
+    fi
+
     echo
 }
 # Displays load average.
 load_average(){
     LOAD_AVERAGE=$(uptime | awk -F'load average: ' '{print $2}')
+    LOAD_1=$(echo "$LOAD_AVERAGE" | cut -d',' -f1)
+    CPU_COUNT=$(nproc)
     echo
     echo "================LOAD AVERAGE==============="
-    echo "Load Average (1, 5, 15 min) : $LOAD_AVERAGE"
+    echo "Load Average : $LOAD_AVERAGE"
+    
+    if (( $(echo "$LOAD_1 > $CPU_COUNT" | bc -l) ))
+    then
+        echo -e "Load Status : ${RED}HIGH LOAD${NC}"
+    else
+        echo -e "Load Status : ${GREEN}NORMAL${NC}"
+    fi
+
     echo
 }
 # Displays network information.
@@ -138,26 +226,27 @@ network_information(){
     INTERFACE_STATUS=$(ip -br addr | grep "eth0" | awk '{print $2}')
     DEFAULT_GATEWAY=$(ip route | grep "^default" | awk '{print $3}')
     echo
-    echo "================NETWORK INFORMATION======="
+    echo "================NETWORK INFORMATION============="
     echo "IP Address      : $IP_ADDRESS"
     echo "Interface       : $INTERFACE"
     echo "Interface Status: $INTERFACE_STATUS"
     echo "Default Gateway : $DEFAULT_GATEWAY"
+    echo
+    if [ "$INTERFACE_STATUS" = "UP" ]
+    then
+        echo -e "Network Status : ${GREEN}CONNECTED${NC}"
+    else
+        echo -e "Network Status : ${RED}DISCONNECTED${NC}"
+    fi
+
+    echo
 }
 
-for cmd in hostname date uptime free lscpu df whoami id ps awk grep cut xargs ip
+for cmd in hostname date uptime free lscpu df whoami id ps awk grep cut xargs ip top bc
 do
     check_command "$cmd"
 done
 
-#system_information
-#memory_information
-#cpu_information
-#disk_information
-#user_information
-#process_information
-#load_average
-#network_information
 
 case "$1" in
     --system)
@@ -184,6 +273,9 @@ case "$1" in
     --network)
       network_information
       ;;
+    --help)
+      help_menu
+      ;;
     --all|"")
      system_information
      memory_information
@@ -195,15 +287,6 @@ case "$1" in
      network_information
      ;;
     *)
-     echo "Usage:"
-     echo "./system_health_monitor.sh --all"
-     echo "./system_health_monitor.sh --system"
-     echo "./system_health_monitor.sh --memory"
-     echo "./system_health_monitor.sh --cpu"
-     echo "./system_health_monitor.sh --disk"
-     echo "./system_health_monitor.sh --user"
-     echo "./system_health_monitor.sh --process"
-     echo "./system_health_monitor.sh --load"
-     echo "./system_health_monitor.sh --network"
+      help_menu
      ;;
 esac
